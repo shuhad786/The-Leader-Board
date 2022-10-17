236146669eb874f701d29c6232fab23e29bf8f8f
Shuhad786 <shuhad.loofer@gmail.com>
2022-10-17 21:06:31 +0200

    Updated for linter errors

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,28 +1,32 @@
 const data = [
-{
-  name: 'first',
-  score: '1',
-},
-{ name: 'second',
-  score: '3',
-},
-{ name: 'bye',
-  score: '10',
-},
-{ name: 'winner',
-  score: '4',
-},
-{ name: 'oh yes',
-  score: '2',
-},
-]; 
+  {
+    name: 'first',
+    score: '1',
+  },
+  {
+    name: 'second',
+    score: '3',
+  },
+  {
+    name: 'bye',
+    score: '10',
+  },
+  {
+    name: 'winner',
+    score: '4',
+  },
+  {
+    name: 'oh yes',
+    score: '2',
+  },
+];
 
 const displayList = () => {
-  const sortedData  = data.sort((a,b) => a.score - b.score);
+  const sortedData = data.sort((a, b) => a.score - b.score);
   const scoreBoardContainer = document.getElementById('scoreBoardContainer');
   scoreBoardContainer.innerHTML = '';
   sortedData.forEach((item, id) => {
-      scoreBoardContainer.innerHTML
+    scoreBoardContainer.innerHTML
       += `
         <div id='scoreBoardItem' class='scoreBoardItem' ${id}>
           <p>${item.name}</p>
@@ -33,4 +37,4 @@ const displayList = () => {
   return sortedData;
 };
 
-export { displayList };
+export default displayList;
